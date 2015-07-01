a9f2072214712e11224ee6ab87484e62a446db8c
Ike Tohru <rokujyouhitomajp@gmail.com>
2015-07-02 00:06:19 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+import sys
+
+from lis import parse
+from lis import eval
+
+
+def run(fp):
+    program_contents = ""
+    while True:
+        read = os.read(fp, 4096)
+        if len(read) == 0:
+            break
+        program_contents += read
+    os.close(fp)
+
+    mainloop(program_contents.split('\n'))
+
+
+def mainloop(statements):
+    for statement in statements:
+        if len(statement) != 0:
+            tokens = parse(statement)
+            print(eval(tokens))
+
+def entry_point(argv):
+    try:
+        filename = argv[1]
+    except IndexError:
+        print "You must supply a filename"
+        return 1
+
+    run(os.open(filename, os.O_RDONLY, 0777))
+    return 0
+
+def target(*args):
+    return entry_point, None
+
+if __name__ == "__main__":
+    entry_point(sys.argv)
